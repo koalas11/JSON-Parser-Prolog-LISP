91541f17814801cb8116723aef1b74af5efa4e00
Koalas <46250330+koalas11@users.noreply.github.com>
2023-01-14 22:28:45 +0100

    Add files via upload

---
+++ jsonparse.lisp
@@ -0,0 +1,204 @@
+;;;; -*- Mode: Lisp -*-
+
+;;;; jsonparse.lisp --
+
+;JSONPARSE
+
+(defun jsonparse (JSONString) 
+  (stringToJSON (fixString JSONString)))
+
+(defun stringToJSON (String)
+  (cond ((string= String "{}") (list 'JSONOBJ))
+        ((string= String "[]") (list 'JSONARRAY))
+        ((and (eql #\{ (firstChar String))
+              (eql #\} (lastChar String)))
+         (cons 'JSONOBJ (getMembers (removeBrackets String))))
+        ((and (eql #\[ (firstChar String))
+              (eql #\] (lastChar String)))
+         (cons 'JSONARRAY (getValues (removeBrackets String))))
+        (t (error "syntax error"))))
+
+(defun getMembers (Members &optional (index 0))
+  (let ((var (position #\, Members :start index)))
+    (handler-case (cons (parseMember (subseq Members 0 var))
+                        (if (null var) nil
+                          (getMembers (subseq Members (+ var 1)))))
+      (error () 
+        (if (null var) (error "syntax error")
+          (getMembers Members (+ var 1)))))))
+
+(defun getValues (Values &optional (index 0))
+  (let ((var (position #\, Values :start index)))
+    (handler-case (cons (parseValue (subseq Values 0 var))
+                        (if (null var) nil
+                          (getValues (subseq Values (+ var 1)))))
+      (error () 
+        (if (null var) (error "syntax error")
+          (getValues Values (+ var 1)))))))
+
+(defun parseMember (Member &optional (index 0))
+  (let ((var (position #\: Member :start index)))
+    (handler-case (if (not (null (getString (subseq Member 0 var))))
+                      (list (getString (subseq Member 0 var))
+                            (parseValue (fixString (subseq Member (+ var 1)))))
+                    (error "syntax error"))
+      (error () 
+        (if (null var) (error "syntax error")
+          (parseMember Member (+ var 1)))))))
+
+(defun parseValue (Value)
+  (cond ((string= Value "true") 'true)
+        ((string= Value "false") 'false)
+        ((string= Value "null") 'null)
+        ((not (null (getString Value)))
+         (getString Value))
+        ((floatp (read-from-string Value))
+         (parse-float (fixString Value)))
+        ((not (null (parse-integer Value :junk-allowed t)))
+         (parse-integer Value))
+        (t (jsonparse Value))))
+
+(defun getString (String)
+  (let* ((var (fixString String))
+         (var2 (length var)))
+    (if (< var2 2) nil
+      (if (and (not (null (search "\"" var :end2 2))) 
+               (not (null (search "\"" var :start2 (- var2 1)))))
+          (replaceQuote (subseq var 1 (- var2 1)))
+        nil))))
+
+(defun replaceQuote (String)
+  (let ((var (search "\\\"" String)))
+    (if (null var) String
+      (concatenate 'string (subseq String 0 var) "\"" (subseq String (+ var 2))))))
+ 
+(defun fixString (String)
+  (string-trim '(#\Space #\Tab #\Newline) String))
+  
+(defun firstChar (String)
+  (char String 0))
+
+(defun lastChar (String)
+  (char String (- (length String) 1)))
+
+(defun removeBrackets (String)
+  (if (< (length String) 2) (error "syntax error")
+    (fixString (subseq String 1 (- (length String) 1)))))
+
+; JSONACCESS
+; esiste solo perchè altrimenti viene applicato &rest ad ogni chiamata ricorsiva
+(defun jsonaccess (json &rest fields)
+  (if (and (eql (first json) 'JSONARRAY) (null fields)) 
+      (error "field empty with starting JSONARRAY")
+    (jsonaccess_ json fields)))
+  
+; sto provando a rimuovere &rest, se non va rimettilo sotto!
+(defun jsonaccess_ (json fields)
+  (let ((field (first fields)))
+    (cond ((null fields) json)
+          ((and (eql (first json) 'JSONOBJ) (stringp field))
+           (jsonaccess_ (accessField (rest json) field) (rest fields)))
+          ((and (eql (first json) 'JSONARRAY) (numberp field))
+           (jsonaccess_ (accessNumber (rest json) field) (rest fields)))
+          (T (error "errore nei fields")))))
+
+
+; se field è una stringa, accedo all'oggetto
+; si potrebbe aggiungere error come access-numer per leggibilita errore
+(defun accessField (json field)
+  (cond ((null json) (error "field ~A not found" field))
+        ((string= (caar json) field) 
+         (second (first json)))
+        (T (accessField (rest json) field))))
+
+
+; se field è un numero, accedo al valore dell'array
+; il secondo cond si può eliminare sostituendo con nth (meglio x leggibilita error)
+(defun accessNumber (json index)
+  (cond ((not (null (nth index json))) 
+         (nth index json))
+        (T (error "index out of bounds"))))
+
+; INPUT/OUTPUT
+
+(defun jsonread (filename)
+  (with-open-file (stream filename
+                          :direction :input
+                          :if-does-not-exist :error)
+    (fileToString stream)))
+
+(defun fileToString (stream)
+  (let ((riga (read-line stream nil 'eof)))
+    (if (eq riga 'eof) ""
+      (concatenate 'string riga (fileToString stream)))))   				
+
+
+; legge oggetto json parsato e lo stampa in formato json
+(defun jsondump (JSON filename)
+  (with-open-file (stream filename
+                          :direction :output
+                          :if-exists :supersede
+                          :if-does-not-exist :create)
+    (jsondump_ stream JSON))
+  filename)
+
+
+(defun jsondump_ (stream JSON &optional (tab 0))
+  (let ((primo (first JSON)))
+    (cond 
+     ((null primo) nil)
+     ((eql primo 'JSONOBJ)
+      (format stream "{" )
+      (jsondumpobj stream (rest JSON) (+ tab 2))
+      (terpri stream)
+      (printTab stream tab)
+      (format stream "}"))
+     ((eql primo 'JSONARRAY)
+      (format stream "[" )
+      (jsondumparray stream (rest JSON) (+ tab 2))
+      (terpri stream)
+      (printTab stream tab)
+      (format stream "]"))
+     (t (error "dump error")))))
+
+
+(defun jsondumpobj (stream JSON tab)
+  (if (null json) nil
+    (let ((pair (first JSON)))
+      (terpri stream)
+      (printTab stream tab)
+      (format stream "~S : " (first pair))
+      (printValue stream (second pair) tab)
+      (if (null (rest JSON)) nil
+        (format stream "," ))
+      (jsondumpobj stream (rest JSON) tab))))
+
+
+
+(defun jsondumparray (stream JSON tab)
+  (if (null json) nil
+    (progn 
+      (terpri stream)
+      (printTab stream tab)
+      (printValue stream (first JSON) tab)
+      (if (null (rest JSON)) nil
+        (format stream "," ))
+      (jsondumparray stream (rest JSON) tab))))
+
+(defun printValue (stream value tab)
+  (if (listp value) (jsondump_ stream value tab)
+    (cond ((eql value 'true)
+           (format stream "true"))
+          ((eql value 'false)
+           (format stream "false"))
+          ((eql value 'null)
+           (format stream "null"))
+          (t (format stream "~S" value)))))
+
+(defun printTab (stream tab)
+  (if (= tab 0) nil
+    (progn 
+      (format stream " ")
+      (printTab stream (- tab 1)))))
+
+;;;; end of file -- jsonparse.lisp --
---
+++ jsonparse.pl
@@ -0,0 +1,335 @@
+%%%% -*- Mode: Prolog -*-
+
+%%%% jsonparse.pl --
+
+% JSONPARSE
+
+% Atom
+jsonparse({}, jsonobj([])) :- !.
+
+jsonparse([], jsonarray([])) :- !.
+
+jsonparse(Atom, JSONResult) :-
+    atom(Atom),
+    catch(term_to_atom(Term, Atom), _, fail),
+    !,
+    jsonparse(Term, JSONResult).
+
+% Stringhe / Codes / Chars
+jsonparse(String, JSONResult) :-
+    string(String),
+    !,
+    catch(term_string(Term, String), _, fail),
+    jsonparse(Term, JSONResult).
+
+jsonparse(Codes, JSONResult) :-
+    is_list(Codes),
+    catch(atom_codes(Atom, Codes), _, fail),
+    jsonparse(Atom, JSONResult),
+    !.
+
+jsonparse(Chars, JSONResult) :-
+    is_list(Chars),
+    catch(atom_chars(Atom, Chars), _, fail),
+    jsonparse(Atom, JSONResult),
+    !.
+
+% Term
+jsonparse(Term, JSONResult) :-
+    compound(Term),
+    !,
+    jsonparse_(Term, JSONResult).
+
+% Jsonparse Reverse
+jsonparse(Result, JSON) :-
+    var(Result),
+    nonvar(JSON),
+    !,
+    jsonparseRev_(Result, JSON).
+
+% Parser
+
+jsonparse_({}, jsonobj([])) :- !.
+
+jsonparse_({Members}, jsonobj(Result)) :-
+    !,
+    Members =.. List,
+    jsonparseobj_(List, Result).
+
+jsonparse_(List, jsonarray(Result)) :-
+    is_list(List),
+    !,
+    jsonparsearray_(List, Result).
+
+jsonparseobj_([':', String, Val], [(String , RValue)]) :-
+    string(String),
+    checkValue(Val, RValue),
+    !.
+
+jsonparseobj_([',', (Str : Val), Rest], [(Str , RVal) | Tail]) :-
+    !,
+    string(Str),
+    checkValue(Val, RVal),
+    Rest =.. LRest,
+    jsonparseobj_(LRest, Tail).
+
+jsonparsearray_([], []) :- !.
+
+jsonparsearray_([Val | Tail], [RVal | RTail]) :-
+    checkValue(Val, RVal),
+    !,
+    jsonparsearray_(Tail, RTail).
+
+% Reverse Parser
+
+jsonparseRev_({}, jsonobj([])) :- !.
+
+jsonparseRev_({Members}, jsonobj(List)) :-
+    !,
+    jsonparseObjRev_(Members, List).
+
+jsonparseRev_(RList, jsonarray(List)) :-
+    !,
+    jsonparseArrayRev_(RList, List).
+
+jsonparseObjRev_(Str : RVal, [',', Str, Val]) :-
+    string(Str),
+    checkValue(RVal, Val),
+    !.
+
+jsonparseObjRev_(Result, [Head]) :-
+    !,
+    functor(Head, ',', 2),
+    Head =.. List,
+    jsonparseObjRev_(Result, List).
+
+jsonparseObjRev_(Result, [Head | Tail]) :-
+    !,
+    Head =.. List,
+    jsonparseObjRev_(TempResult1, List),
+    jsonparseObjRev_(TempResult2, Tail),
+    Result =.. [',', TempResult1, TempResult2].
+
+jsonparseArrayRev_([], []) :- !.
+
+jsonparseArrayRev_(Result, [Head | Tail]) :-
+    !,
+    checkValue(Pair, Head),
+    jsonparseArrayRev_(ResultTail, Tail),
+    append([Pair], ResultTail, Result).
+
+% checkValue
+
+checkValue(Str, Str) :-
+    string(Str),
+    !.
+
+checkValue(Num, Num) :-
+    number(Num),
+    !.
+
+checkValue('true', 'true') :- !.
+
+checkValue('false', 'false') :- !.
+
+checkValue('null', 'null') :- !.
+
+checkValue(JSON, Result) :-
+    nonvar(JSON),
+    !,
+    jsonparse_(JSON, Result).
+
+checkValue(Result, JSON) :-
+    !,
+    nonvar(JSON),
+    jsonparseRev_(Result, JSON).
+
+% JSONACCESS
+
+jsonaccess(jsonarray(_), [], _) :- !, fail.
+
+jsonaccess(JSON, Fields, Result) :-
+    nonvar(JSON),
+    !,
+    jsonaccess_(JSON, Fields, Result).
+
+jsonaccess_(jsonobj(List), String, Result) :-
+    string(String),
+    !,
+    member((String , Result), List),
+    !.
+
+jsonaccess_(jsonobj(List), Var, Result) :-
+    var(Var),
+    !,
+    member((Var , Result), List).
+
+jsonaccess_(jsonarray(List), Index, Result) :-
+    integer(Index),
+    !,
+    nth0(Index, List, Result).
+
+jsonaccess_(jsonarray(List), Var, Result) :-
+    var(Var),
+    !,
+    nth0(Var, List, Result).
+
+jsonaccess_(Obj, [], Obj) :- !.
+
+jsonaccess_(Obj, [Field | OtherFields], Result) :-
+    !,
+    jsonaccess_(Obj, Field, TRes),
+    jsonaccess_(TRes, OtherFields, Result).
+
+% INPUT
+% JSONREAD
+
+jsonread(FileName, JSON) :-
+    nonvar(FileName),
+    catch(open(FileName, read, In), _, fail),
+    catch(
+        (
+        read_string(In, _, String),
+        jsonparse(String, JSON)
+    ),
+        _,
+        (close(In), fail)
+    ),
+    !,
+    close(In).
+
+% Caso per gestire la presenza di \/ nel file
+
+jsonread(FileName, JSON) :-
+    nonvar(FileName),
+    catch(open(FileName, read, In), _, fail),
+    catch(
+        (
+        get_char(In, Char),
+        jsonread(In, Char, Chars),
+        jsonparse(Chars, JSON)
+    ),
+        _,
+        (close(In), fail)
+    ),
+    !,
+    close(In).
+
+jsonread(_, end_of_file, []) :- !.
+
+jsonread(Stream, '\\', Chars) :-
+    get_char(Stream, '/'),
+    !,
+    Chars = ['/' | Rest],
+    get_char(Stream, Next),
+    jsonread(Stream, Next, Rest).
+
+jsonread(Stream, Char, Chars) :-
+    !,
+    Chars = [Char | Rest],
+    get_char(Stream, Next),
+    jsonread(Stream, Next, Rest).
+
+% OUTPUT
+% JSONDUMP
+
+jsondump(JSON, File) :-
+    nonvar(JSON),
+    atom(File),
+    open(File, write, Out),
+    JSON =.. ListJSON,
+    jsondump_(ListJSON, Out, 0),
+    close(Out).
+
+jsondump_([jsonobj, {}], Out, _) :-
+    !,
+    write(Out, "{}").
+
+jsondump_([jsonarray, []], Out, _) :-
+    !,
+    write(Out, "[]").
+
+jsondump_([jsonobj, List], Out, Index) :-
+    !,
+    writeln(Out, "{"),
+    Index1 is Index + 2,
+    jsondumpobj_(List, Out, Index1),
+    tab(Out, Index),
+    write(Out, "}").
+
+jsondump_([jsonarray, List], Out, Index) :-
+    !,
+    writeln(Out, "["),
+    Index1 is Index + 2,
+    jsondumparray_(List, Out, Index1),
+    tab(Out, Index),
+    write(Out, "]").
+
+%OBJ DUMP
+
+jsondumpobj_([',', Str, Value], Out, Index) :-
+    checkValue(Value, _),
+    !,
+    tab(Out, Index),
+    format(Out, '"~w" : ', [Str]),
+    printValue(Out, Value).
+
+jsondumpobj_([',', Str, Value], Out, Index) :-
+    !,
+    tab(Out, Index),
+    format(Out, '"~w" : ', [Str]),
+    Value =.. ListValue,
+    jsondump_(ListValue, Out, Index).
+
+jsondumpobj_([Head], Out, Index) :-
+    !,
+    Head =.. ListHead,
+    jsondumpobj_(ListHead, Out, Index),
+    nl(Out).
+
+jsondumpobj_([Head | Tail], Out, Index) :-
+    !,
+    Head =.. ListHead,
+    jsondumpobj_(ListHead, Out, Index),
+    writeln(Out, ","),
+    jsondumpobj_(Tail, Out, Index).
+
+% ARRAY DUMP
+
+jsondumparray_([Head], Out, Index) :-
+    !,
+    tab(Out, Index),
+    jsondumparray_(Head, Out, Index),
+    nl(Out).
+
+jsondumparray_([Head | Tail], Out, Index) :-
+    !,
+    tab(Out, Index),
+    jsondumparray_(Head, Out, Index),
+    writeln(Out, ","),
+    jsondumparray_(Tail, Out, Index).
+
+jsondumparray_(Value, Out, _) :-
+    checkValue(Value, _),
+    !,
+    format(Out, '"~w"', [Value]).
+
+jsondumparray_(Value, Out, Index) :-
+    !,
+    Value =.. List,
+    jsondump_(List, Out, Index).
+
+% printValue
+
+printValue(Out, Str) :-
+    string(Str),
+    !,
+    write(Out, "\""),
+    write(Out, Str),
+    write(Out, "\"").
+
+printValue(Out, Value) :-
+    !,
+    write(Out, Value).
+
+%%%% end of file -- jsonparse.pl --
